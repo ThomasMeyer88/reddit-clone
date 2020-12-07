4dfa19463c6e40435970b9bb82fc7f1cd6e9e030
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-12-07 12:10:16 -0600

    header and post components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent,
     UserLoginComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    HeaderComponent,
+    PostListComponent,
+    CreatePostComponent,
+    ViewPostComponent
   ],
   imports: [
     BrowserModule,
