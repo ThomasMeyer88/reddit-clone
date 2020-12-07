57ec112dda1df22c11206bfe9cbf8e2ecc79351a
Thomas Meyer <thomasmmeyer88@gmail.com>
2020-12-07 12:08:07 -0600

    user components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    UserLoginComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
