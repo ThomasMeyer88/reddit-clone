0d2e8261f87c52b812f4b33b40b9f4784b7fbaf5
Thomas Meyer <thomasmmeyer88@gmail.com>
2021-01-11 11:36:50 -0600

    Update httpclient.service.ts

--- src/app/services/httpclient.service.ts
+++ src/app/services/httpclient.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {User} from '../models/user';
 import { CustomResponse } from '../models/customResponse';
 
-@Injectable({
-  providedIn: 'root'
-})
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +11,10 @@ export class HttpClientService {
 
   constructor(private httpClient:HttpClient) {}
 
+  headers = new HttpHeaders({ Authorization: 'Basic ' + btoa('javainuse' + ':' + 'password') });
+
   public createEmployee(user: User) {
+    const h = this.headers;
     return this.httpClient.post<CustomResponse>("http://localhost:8080/user/create", user);
   }
 }
